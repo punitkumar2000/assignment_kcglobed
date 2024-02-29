62a268eb738a78c14d09322f3e8c5c96dbde0dab
PUNIT KUMAR <76520175+punitkumar2000@users.noreply.github.com>
2024-02-29 10:00:30 +0530

    Update settings.py
    change in setting file

--- E_commerce/kcglobed_world/kcglobed_world/settings.py
+++ E_commerce/kcglobed_world/kcglobed_world/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -139,8 +139,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
-from pathlib import Path
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Define the directory where uploaded media files will be stored
 
